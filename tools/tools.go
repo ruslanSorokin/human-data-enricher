a28388b74763e7b6065952677ebccfebf8b7e5d5
Ruslan Sorokin <strawberryladder@gmail.com>
2023-09-12 19:49:08 +0300

    build(deps/go): add linters via go tools package

--- tools/tools.go
+++ tools/tools.go
@@ -2,3 +2,10 @@
 // +build tools
 
 package tools
+
+import (
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/incu6us/goimports-reviser/v3"
+	_ "github.com/segmentio/golines"
+	_ "mvdan.cc/gofumpt"
+)
